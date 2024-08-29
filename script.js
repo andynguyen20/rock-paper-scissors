8be6181f0645c9cf537fb9574f061c4dc50b8dca
Andy Nguyen <andynguyenn20@gmail.com>
2024-08-28 23:58:11 -0400

    Finished creating single round logic

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-function getComputerChoice(initialNumber) {
-    let number = Math.random(initialNumber);
+function getComputerChoice() {
+    let number = Math.random();
     if (number < 0.3333) {
-        let choice = "rock";
-        console.log(choice);
+    return "rock";
     } else if (0.3333 < number && number < 0.6666) {
-        let choice = "paper";
-        console.log(choice);
+    return "paper";
     } else {
-        let choice = "scissors";
-        console.log(choice);
+        return "scissors";
     }
 }
-let number = 10;
-getComputerChoice(number);
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
 
 function getHumanChoice() {
-
     while(true) {
-    const status = prompt("rock, paper, scissors");
+    const status = prompt("rock, paper, scissors").toLowerCase();
     if (status === "rock" || status === "paper" || status === "scissors") {
-        return console.log(status);
+        return status;
     } else {
      alert("not a valid option, please try again");
     }
 }
 }
-getHumanChoice();
+let humanChoice = getHumanChoice();
+console.log(humanChoice);
 
 humanScore = 0;
 computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        alert ("Tie!! No one gets a point");
+    } else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
+        alert ("You win. +1 point.");
+        humanScore += 1;
+    } else 
+        alert ("You lose. +1 point for computer.")
+        computerScore += 1;
+}
+
+let outcome = playRound(humanChoice, computerChoice);

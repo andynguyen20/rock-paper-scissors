3fc57a96e5f032921e09222b285b97e66538f157
Andy Nguyen <andynguyenn20@gmail.com>
2024-08-28 22:16:01 -0400

    Created computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+function getComputerChoice(initialNumber) {
+    let number = Math.random(initialNumber);
+    if (number < 0.3333) {
+        let choice = "rock";
+        console.log(choice);
+    } else if (0.3333 < number && number < 0.6666) {
+        let choice = "paper";
+        console.log(choice);
+    } else {
+        let choice = "scissors";
+        console.log(choice);
+    }
+}
+let number = 10;
+getComputerChoice(number);
+
+function getHumanChoice(input) {
+    prompt("Rock, Paper, Scissors");
+}
+let input;
+getHumanChoice(input);
